eb1bf46861f43a472ea12c1dc4ea3138f21b6d4e
Akshay Bhamse <iamguddu37@gmail.com>
2024-05-25 17:30:25 +0530

    Initial Commit

---
+++ Darkweb/DarkwebApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkwebApp.swift
+//  Darkweb
+//
+//  Created by Akshay Bhasme on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DarkwebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
